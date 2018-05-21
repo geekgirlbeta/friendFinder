361567d774521c93c11854357f71f8086c386ef4
Stacy M <geekgirlbeta@gmail.com>
2018-05-21 10:36:54 -0400

    fixes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,12 +1,27 @@
 var friendsArray = [{
-        name: "Stacy",
-        photo: "https://geekgirlbeta.files.wordpress.com/2015/02/geekgirlbetalogoblog.jpg",
-        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        name: "Flik",
+        photo: "https://vignette.wikia.nocookie.net/disney/images/7/77/Flik_Character_Model.png/revision/latest?cb=20160827035036",
+        scores: [5, 5, 5, 3, 1, 1, 1, 1, 3, 4]
     },
     {
-        name: "Aggretsuko",
-        photo: "https://atomsmashermusicblog.files.wordpress.com/2018/04/31170342_10155983919060081_5011954331641970688_n.jpg?w=471&h=471",
-        scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+        name: "Charlotte",
+        photo: "https://cosmos-images2.imgix.net/file/spina/photo/13566/180112-web-full.jpg?ixlib=rails-2.1.4&auto=format&ch=Width%2CDPR&fit=max&w=835",
+        scores: [5, 2, 5, 5, 1, 2, 1, 3, 3, 5]
+    },
+    {
+        name: "Absolem the Caterpillar",
+        photo: "https://i.pinimg.com/originals/48/95/6a/48956ad6e80e5a423b7f5253f19416b6.jpg",
+        scores: [5, 1, 1, 3, 1, 3, 1, 5, 1, 1]
+    },
+    {
+        name: "Jiminy Cricket",
+        photo: "https://vignette.wikia.nocookie.net/disney/images/b/bb/Jiminyumbrella.png/revision/latest?cb=20120507024442",
+        scores: [5, 2, 3, 1, 3, 2, 2, 1, 1, 1]
+    },
+    {
+        name: "Jewel Beetle",
+        photo: "https://cdn.shopify.com/s/files/1/1271/9297/products/image_6717ea29-2ea1-413a-8fef-af87118522c2_grande.jpg?v=1506023386",
+        scores: [1, 1, 2, 1, 2, 1, 1, 2, 1, 5]
     }
 ];
 
